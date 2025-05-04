74f3c1b441fdcf46f1b89691ee8538a986b44921
Satya Prakash Jagata <connectwithsatya333@gmail.com>
2025-05-04 10:50:38 +0530

    Day 2

---
+++ jsprac.js
@@ -0,0 +1,39 @@
+
+// Using let as variable declaration
+/*let string = 'Hello World'
+console.log(string[2])
+console.log(string.length)
+console.log(string[string.length-1])
+
+console.log(Boolean("1"))
+let userEmail = 'connectwithsatya333@gmail.com';
+
+let age = 18;
+
+console.log(age);
+
+console.log(userEmail);
+
+age = age + 1;
+age = 50
+
+console.log(age); */
+
+
+//Using Const as variable declaration
+/*const nameUser = `Hello`
+
+const ageNa = 100
+
+console.log(nameUser)
+console.log(ageNa)
+
+ageNa = 50;
+console.log(ageNa)*/
+
+//Task Convert AUD to USD
+/*let USD = 1200;
+
+let AUD = USD * 1.5;
+
+console.log("USD to ASD is ", AUD);*/
