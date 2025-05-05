24c0f1f18ee7ce097954f77a6e467744eda68f39
Satya Prakash Jagata <connectwithsatya333@gmail.com>
2025-05-05 20:32:23 +0530

    Day 3

--- jsprac.js
+++ jsprac.js
@@ -1,4 +1,3 @@
-
 // Using let as variable declaration
 /*let string = 'Hello World'
 console.log(string[2])
@@ -19,7 +18,6 @@ age = 50
 
 console.log(age); */
 
-
 //Using Const as variable declaration
 /*const nameUser = `Hello`
 
@@ -37,3 +35,71 @@ console.log(ageNa)*/
 let AUD = USD * 1.5;
 
 console.log("USD to ASD is ", AUD);*/
+
+/*let hasMembership = false;
+let signedIn = true;
+
+if (hasMembership === true && signedIn === true) {
+  console.log("Show the tutorial");
+} else if (signedIn === true && hasMembership === false) {
+  console.log("Upgrade Now");
+} else {
+  console.log("User has no Membership");
+}*/
+
+/*let age = 17
+
+if(age !== 18){
+
+console.log(`${age} is greater than 18`)
+}
+else if(age === 18){
+    console.log(`${age} is equals to 18`)
+}
+else{
+    console.log(`${age} is less than 18`)
+}*/
+
+/*Check if a number is positive
+If a number is greater than 0, print "Positive".
+
+Check if a user is an adult
+If the age is 18 or more, print "Adult".
+
+Check if a string contains a specific word
+If a string includes the word "JavaScript", print "Found".
+
+Check if a number is divisible by 5
+If a number is divisible by 5, print "Divisible by 5".
+
+Check if a password is long enough
+If the password is 8 characters or more, print "Valid password".
+
+// let isNumber = 6;
+
+// isNumber > 0 ? console.log(`${isNumber} is positive`): console.log(`${isNumber} is negative`);
+
+let isNumber = 100;
+
+isNumber % 5 === 0 ? console.log(`${isNumber} is diisible by 5`): console.log(`${isNumber} is not diisible by 5`); */
+
+//Check if a user is an adult If the age is 18 or more, print "Adult
+
+// let age = 19;
+
+// age >= 18 ? console.log("Adult"): console.log("Minor");
+
+//Check if a password is long enough If the password is 8 characters or more, print "Valid password"
+
+// let password = "satya@0123";
+
+// password.length >= 8
+//   ? console.log("Valid password")
+//   : console.log("Invalid password");
+
+
+let loggedIn = true
+
+let hasMembership = true
+
+loggedIn && hasMembership ? console.log("Show Video"): console.log(" don't Show Video");
